493221d156e3fc74c679ddefcc5b64b7574e7081
MySnowWork <126544109+MySnowWork@users.noreply.github.com>
2023-03-15 12:46:46 +0530

    Update streamlit_app.py
    created function to get fruitchoice

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+ def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
 # New section to display fruity vice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
@@ -36,16 +41,10 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    #streamlit.write('The user entered ', fruit_choice)
-  # import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-  # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
-
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
-  streamlit.error()
+   streamlit.error()
     
 # add fruits text box
 add_my_fruit=streamlit.text_input('What fruit would you like to add?','jackfruit')
